796e1cc78fabe497075c13536dca132eb0df56b5
Adolphus Miracle <miraboy13@gmail.com>
2023-12-28 23:49:09 +0100

    feat: implemented job details screen

--- components/jobdetails/tabs/Tabs.jsx
+++ components/jobdetails/tabs/Tabs.jsx
@@ -1,12 +1,35 @@
 import React from 'react'
-import { View, Text } from 'react-native'
-
+import { View, Text, TouchableOpacity, FlatList } from 'react-native'
 import styles from './tabs.style'
+import { SIZES } from '../../../constants'
 
-const Tabs = () => {
+const TabButton = ({label, activeTab, onHandleSearch}) => {
   return (
-    <View>
-      <Text>Tabs</Text>
+    <TouchableOpacity
+      style={styles.btn(activeTab, label)}
+      onPress={() => onHandleSearch(label)}
+    >
+      <Text style={styles.btnText(activeTab, label)}>{label}</Text>
+    </TouchableOpacity>
+  )
+}
+const Tabs = ({tabs, activeTab, setActiveTab}) => {
+  return (
+    <View style={styles.container}>
+      <FlatList
+        data={tabs}
+        renderItem={({item}) => (
+          <TabButton
+            label={item}
+            activeTab={activeTab}
+            onHandleSearch={setActiveTab}
+          />       
+        )}
+        keyExtractor={item => item}
+        contentContainerStyle={{columnGap: SIZES.small}}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+       />
     </View>
   )
 }

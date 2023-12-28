dec116edb3f1c4158e22678dd0b520934384b2ac
Adolphus Miracle <miraboy13@gmail.com>
2023-12-28 16:12:09 +0100

    feat: added needed packages for project

---
+++ app/index.js
@@ -0,0 +1,15 @@
+import { View, Text, SafeAreaView } from 'react-native'
+import {Stack, useRouter} from 'expo-router'
+import {useState} from 'react'
+
+import { COLORS, icons, images, SIZES } from '../constants' 
+
+const Home = () => {
+  return (
+    <SafeAreaView style={{flex: 1, backgroundColor:COLORS.lightWhite}}>
+      
+      
+    </SafeAreaView>
+  ) 
+}
+export default Home

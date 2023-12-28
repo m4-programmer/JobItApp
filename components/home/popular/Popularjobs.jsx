ceb32513b808c3b1ad113f9f0974a3db3e70c9bc
Adolphus Miracle <miraboy13@gmail.com>
2023-12-28 18:19:33 +0100

    feat: finish implementing popular jobs component and get dynamic data from api

--- components/home/popular/Popularjobs.jsx
+++ components/home/popular/Popularjobs.jsx
@@ -4,11 +4,12 @@ import { useRouter } from 'expo-router'
 import styles from './popularjobs.style'
 import { COLORS, SIZES } from '../../../constants'
 import PopularJobCard from '../../common/cards/popular/PopularJobCard'
+import useFetch from '../../../hook/useFetch'
 
 const Popularjobs = () => {
   const router = useRouter();
-  const isLoading = false;
-  const error = false;
+  
+  const {data, isLoading, error} = useFetch('search', {query: 'React developer', num_pages: '1'})
 
   return (
     <View style={styles.container}>
@@ -21,10 +22,12 @@ const Popularjobs = () => {
 
       <View style={styles.cardsContainer}>
         {
-          isLoading ? (<ActivityIndicator size="large" color={COLORS.primary} />) 
-          : error ? (<Text>Something went wrong</Text>) : (
+          isLoading ? 
+          (<ActivityIndicator size="large" color={COLORS.primary} />) 
+          : error ? (<Text>Something went wrong</Text>) : 
+          (
             <FlatList
-          data={[1, 2, 3, 4, 5, 6, 7, 8, 9]}
+          data={data}
           renderItem={({item}) => (
             <PopularJobCard
               item={item}
